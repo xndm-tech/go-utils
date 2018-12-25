8f4c37ce5c740ccbe524f930df0324eecf5d52f1
xndm_recommend <1050099567@qq.com>
2018-12-25 17:46:29 +0800

    mysql update

--- mysqls/interface.go
+++ mysqls/interface.go
@@ -5,10 +5,18 @@ package mysqls
 */
 import (
 	"database/sql"
+	"time"
+
+	"github.com/xndm-recommend/go-utils/errors_"
+	"github.com/xndm-recommend/go-utils/tools"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/xndm-recommend/go-utils/conf_read"
+	"github.com/xndm-recommend/go-utils/config"
 )
+
+type MysqlMethod interface {
+	GetDbConnFromConf(c *config.ConfigEngine, name string)
+}
 
 type MysqlDbInfo struct {
 	SqlDataDb *sql.DB
@@ -17,9 +25,26 @@ type MysqlDbInfo struct {
 	DbTimeOut int
 }
 
-func GetDbConnFromConf(this *conf_read.ConfigEngine, sectionName string) *MysqlDbInfo {
-	DbInfo := new(MysqlDbInfo)
-	sLogin := getSqlDataFromConf(this, sectionName)
-	DbInfo.createDatabaseConns(sLogin)
-	return DbInfo
+func getMySqlLoginStr(data *config.MysqlDbYamlData) string {
+	return tools.JoinStrByBuf(data.User, ":",
+		data.Password, "@tcp(", data.Host, ":",
+		data.Port, ")/", data.Db_name, "?charset=utf8")
+}
+
+func (this *MysqlDbInfo) createDatabaseConns(login *config.MysqlDbYamlData) {
+	db, err := sql.Open("mysql", getMySqlLoginStr(login))
+	errors_.CheckFatalErr(err)
+	db.SetConnMaxLifetime(time.Duration(login.Time_out) * time.Second)
+	db.SetMaxOpenConns(login.Max_conns)
+	db.SetMaxIdleConns(login.Max_conns)
+	err = db.Ping()
+	errors_.CheckFatalErr(err)
+	this.SqlDataDb = db
+	this.TableName = login.Table_name
+	this.MaxConns = login.Max_conns
+	this.DbTimeOut = login.Time_out
+}
+
+func (this *MysqlDbInfo) GetDbConnFromConf(c *config.ConfigEngine, name string) {
+	this.createDatabaseConns(c.GetMySqlFromConf(name))
 }
