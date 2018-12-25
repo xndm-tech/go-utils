8879ee343b85f1e7c19707e0f899b11fe040ad88
xndm_recommend <1050099567@qq.com>
2018-12-25 18:53:01 +0800

    config struct rename

--- mssqls/interface.go
+++ mssqls/interface.go
@@ -1,13 +1,25 @@
 package mssqls
 
+/*
+有关sqlServer数据库连接的封装
+*/
 import (
 	"database/sql"
 	"fmt"
 	"time"
 
-	"github.com/xndm-recommend/go-utils/config"
 	"github.com/xndm-recommend/go-utils/errors_"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/xndm-recommend/go-utils/conf_read"
 )
+
+type MssqlDbInfo struct {
+	SqlDataDb *sql.DB
+	TableName map[string]string
+	MaxConns  int
+	DbTimeOut int
+}
 
 func getMssqlLoginStr(section *config.MssqlDbYamlData) string {
 	//连接字符串
@@ -27,4 +39,11 @@ func (this *MssqlDbInfo) createMssqlConns(login *config.MssqlDbYamlData) {
 	this.TableName = login.Table_name
 	this.MaxConns = login.Max_conns
 	this.DbTimeOut = login.Time_out
+}
+
+func GetMssqlConnFromConf(this *conf_read.ConfigEngine, sectionName string) *MssqlDbInfo {
+	DbInfo := new(MssqlDbInfo)
+	sLogin := getMssqlDataFromConf(this, sectionName)
+	DbInfo.createMssqlConns(sLogin)
+	return DbInfo
 }
