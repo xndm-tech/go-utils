5e45ed073e55e997f05861307127b1c8368096a9
xndm_recommend <1050099567@qq.com>
2019-01-04 11:12:59 +0800

    mysql update

--- mysqls/mysql_test.go
+++ mysqls/mysql_test.go
@@ -1,7 +1,11 @@
 package mysqls
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+
+	"github.com/xndm-recommend/go-utils/tools"
 
 	"github.com/xndm-recommend/go-utils/config"
 	"github.com/xndm-recommend/go-utils/errors_"
@@ -19,8 +23,24 @@ func TestMysqlDbInfo_QueryIdList(b *testing.T) {
 	dbinfo := mysqls.MysqlDbInfo{}
 	dbinfo.GetDbConnFromConf(&c, "Comic_data")
 
-	ids := dbinfo.QueryIdList("select cartoon_id,settled_rate from cartoon limit 1")
+	ids := dbinfo.QueryIdList("select cartoon_id from cartoon limit 1")
 	b.Log(ids)
 
-	//b.Logf("hit: %d miss: %d ratio: %f", hit, miss, float64(hit)/float64(miss))
+	sql := "Select cartoon_id From cartoon limit 1"
+	a := strings.ToLower(sql)
+
+	a = tools.SplitStrSep(a, "select", "from")
+	fmt.Println(tools.ContainStrNum(a, ","))
+	//fmt.Println(strings.Contains(a, ","))
+}
+
+func TestMysqlDbInfo_QueryStruct(t *testing.T) {
+	c := config.ConfigEngine{}
+	err := c.Load(Config_path)
+	errors_.CheckCommonErr(err)
+	dbinfo := mysqls.MysqlDbInfo{}
+	dbinfo.GetDbConnFromConf(&c, "Comic_data")
+
+	dbinfo.QueryStruct("select cartoon_id,cartoon_id from cartoon limit 1")
+	t.Log(ids)
 }
--- mysqls/sql.go
+++ mysqls/sql.go
@@ -1,34 +1,26 @@
 package mysqls
 
 import (
+	"strings"
+
 	"github.com/xndm-recommend/go-utils/errors_"
+	"github.com/xndm-recommend/go-utils/tools"
 )
 
 // 封装用的较多的增删改查功能
-func (this *MysqlDbInfo) QueryIdList(sql string) (ids []interface{}) {
-	rows, err := this.SqlDataDb.Query(sql)
-	if err != nil {
-		errors_.CheckCommonErr(err)
-		return ids
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var tmpId string
-		err := rows.Scan(&tmpId)
-		errors_.CheckCommonErr(err)
-		if nil == err {
-			ids = append(ids, tmpId)
-		}
-	}
-	return ids
+func checkNumSql(sql string) int {
+	sql_low := strings.ToLower(sql)
+	return tools.ContainStrNum(tools.SplitStrSep(sql_low, "select", "from"), ",")
 }
 
-// 封装用的较多的增删改查功能
-func (this *MysqlDbInfo) QueryStruct(sql string) (ids []interface{}) {
+func (this *MysqlDbInfo) QueryIdList(sql string) (ids []interface{}) {
+	if checkNumSql(sql) != 0 {
+		return
+	}
 	rows, err := this.SqlDataDb.Query(sql)
 	if err != nil {
 		errors_.CheckCommonErr(err)
-		return ids
+		return
 	}
 	defer rows.Close()
 	for rows.Next() {
@@ -39,5 +31,23 @@ func (this *MysqlDbInfo) QueryStruct(sql string) (ids []interface{}) {
 			ids = append(ids, tmpId)
 		}
 	}
-	return ids
+	return
+}
+
+func (this *MysqlDbInfo) QueryStruct(sql string, out ...*interface{}) {
+	rows, err := this.SqlDataDb.Query(sql)
+	if err != nil {
+		errors_.CheckCommonErr(err)
+		return
+	}
+	defer rows.Close()
+	for _, o := range out {
+		var tmp string
+		err = rows.Scan(&tmp)
+		errors_.CheckCommonErr(err)
+		if nil == err {
+			*o = tmp
+		}
+	}
+	return
 }
