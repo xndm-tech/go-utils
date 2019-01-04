1f477fdc9227bfb5db2c2a05cd19e2aeb335628c
xndm_recommend <1050099567@qq.com>
2019-01-04 10:28:37 +0800

    string update

--- tools/strings.go
+++ tools/strings.go
@@ -96,3 +96,7 @@ func JoinStrByBufSep(sep string, str ...string) string {
 func SplitStrSep(str string, sep1, sep2 string) string {
 	return strings.Split(strings.Split(str, sep1)[1], sep2)[0]
 }
+
+func ContainStrNum(str string, sep string) int {
+	return len(strings.Split(str, sep)) - 1
+}
