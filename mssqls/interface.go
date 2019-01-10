68defd0c9655f6b17ad85f655b045fb668c2b323
xndm_recommend <1050099567@qq.com>
2019-01-10 12:50:50 +0800

    sql to sqlx

--- mssqls/interface.go
+++ mssqls/interface.go
@@ -4,9 +4,10 @@ package mssqls
 有关sqlServer数据库连接的封装
 */
 import (
-	"database/sql"
 	"fmt"
 	"time"
+
+	"github.com/jmoiron/sqlx"
 
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/xndm-recommend/go-utils/config"
@@ -15,14 +16,14 @@ import (
 
 type MssqlMethod interface {
 	GetMssqlConnFromConf(c *config.ConfigEngine, name string)
-	QueryIdList(sql string)
-	QueryIdListLen(sql string, len int)
-	QueryStruct(sql string, pars ...interface{})
-	QueryIdMap(sql string)
+	QueryIdList(sql string, para ...string) (ids []string, err error)
+	QueryIdIntList(sql string, para ...string) (ids []int, err error)
+	QueryStruct(sql string, dest ...interface{}) (err error)
+	QueryIdMap(sql string) (result map[string]string, err error)
 }
 
 type MssqlDbInfo struct {
-	SqlDataDb *sql.DB
+	SqlDataDb *sqlx.DB
 	TableName map[string]string
 	MaxConns  int
 	DbTimeOut int
@@ -35,7 +36,7 @@ func getMssqlLoginStr(section *config.MssqlDbData) string {
 }
 
 func (this *MssqlDbInfo) createMssqlConns(login *config.MssqlDbData) {
-	db, err := sql.Open("mssql", getMssqlLoginStr(login))
+	db, err := sqlx.Open("mssql", getMssqlLoginStr(login))
 	errors_.CheckFatalErr(err)
 	db.SetConnMaxLifetime(time.Duration(login.Time_out) * time.Second)
 	db.SetMaxOpenConns(login.Max_conns)
--- mssqls/query.go
+++ mssqls/query.go
@@ -1,84 +1,35 @@
 package mssqls
 
 import (
-	"strings"
-
 	"github.com/xndm-recommend/go-utils/errors_"
-	"github.com/xndm-recommend/go-utils/tools"
 )
 
-// 封装用的较多的增删改查功能
-func checkNumSql(sql string) int {
-	sql_low := strings.ToLower(sql)
-	return tools.ContainStrNum(tools.SplitStrSep(sql_low, "select", "from"), ",")
-}
-
-func (this *MssqlDbInfo) QueryIdList(sql string) (ids []string, err error) {
-
-	rows, err := this.SqlDataDb.Query(sql)
+//http://jmoiron.github.io/sqlx/
+func (this *MssqlDbInfo) QueryIdList(sql string, para ...string) (ids []string, err error) {
+	err = this.SqlDataDb.Select(&ids, sql, para)
 	if err != nil {
-		rows, err = this.SqlDataDb.Query(sql)
+		err := this.SqlDataDb.Select(&ids, sql, para)
 		if err != nil {
 			errors_.CheckCommonErr(err)
 			return
 		}
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var tmpId string
-		err := rows.Scan(&tmpId)
-		errors_.CheckCommonErr(err)
-		if nil == err {
-			ids = append(ids, tmpId)
-		}
-	}
 	return
 }
 
-func (this *MssqlDbInfo) QueryIdIntList(sql string) (ids []int, err error) {
-	rows, err := this.SqlDataDb.Query(sql)
+func (this *MssqlDbInfo) QueryIdIntList(sql string, para ...string) (ids []int, err error) {
+	err = this.SqlDataDb.Select(&ids, sql, para)
 	if err != nil {
-		rows, err = this.SqlDataDb.Query(sql)
+		err := this.SqlDataDb.Select(&ids, sql, para)
 		if err != nil {
 			errors_.CheckCommonErr(err)
 			return
 		}
 	}
-	defer rows.Close()
-	for rows.Next() {
-		var tmpId int
-		err := rows.Scan(&tmpId)
-		errors_.CheckCommonErr(err)
-		if nil == err {
-			ids = append(ids, tmpId)
-		}
-	}
-	return
-}
-func (this *MssqlDbInfo) QueryIdListLen(sql string, len int) (ids []string, err error) {
-	stmt, err := this.SqlDataDb.Prepare(sql + " LIMIT ?")
-	defer stmt.Close()
-	errors_.CheckCommonErr(err)
-	row, err := stmt.Query(len)
-	if err != nil {
-		row, err = stmt.Query(len)
-		if err != nil {
-			errors_.CheckCommonErr(err)
-			return
-		}
-	}
-	defer row.Close()
-	errors_.CheckCommonErr(err)
-	for row.Next() {
-		var tmpId string
-		err = row.Scan(&tmpId)
-		errors_.CheckCommonErr(err)
-		ids = append(ids, tmpId)
-	}
 	return
 }
 
-func (this *MssqlDbInfo) QueryStruct(sql string, pars ...interface{}) (err error) {
+func (this *MssqlDbInfo) QueryStruct(sql string, dest ...interface{}) (err error) {
 	rows, err := this.SqlDataDb.Query(sql)
 	if err != nil {
 		rows, err = this.SqlDataDb.Query(sql)
@@ -89,13 +40,13 @@ func (this *MssqlDbInfo) QueryStruct(sql string, pars ...interface{}) (err error
 	}
 	defer rows.Close()
 	rows.Next()
-	err = rows.Scan(pars...)
+	err = rows.Scan(dest...)
 	errors_.CheckCommonErr(err)
 	return
 }
 
-func (this *MssqlDbInfo) QueryIdMap(sql string) (out map[string]string, err error) {
-	out = make(map[string]string, 0)
+func (this *MssqlDbInfo) QueryIdMap(sql string) (result map[string]string, err error) {
+	result = make(map[string]string, 0)
 	// 查询数据
 	var key, val string
 	row, err := this.SqlDataDb.Query(sql)
@@ -111,7 +62,7 @@ func (this *MssqlDbInfo) QueryIdMap(sql string) (out map[string]string, err erro
 	for row.Next() {
 		err = row.Scan(&key, &val)
 		errors_.CheckCommonErr(err)
-		out[key] = val
+		result[key] = val
 	}
 	return
 }
