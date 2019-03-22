9af398387523be27c22bb4ea35a52ab36127aa12
xndm-recommend <1050099567@qq.com>
2019-03-22 19:55:14 +0800

    error

--- errors_/raven.go
+++ errors_/raven.go
@@ -13,7 +13,7 @@ func SentryCaptureError(err error) {
 func CheckErrSendEmail(err error) {
 	if err != nil {
 		errDetail := errors.WithStack(err)
-		seelog.Error(errDetail)
+		seelog.Errorf("%+v", errDetail)
 		raven.CaptureError(errDetail, nil)
 	}
 }
