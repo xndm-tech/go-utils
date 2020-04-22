c6f3c9ecefa7c4238a80fdb8eda7a05e4ae63344
xndm-recommend <1050099567@qq.com>
2020-04-22 20:03:56 +0800

    add: common req

--- common/base/define.go
+++ common/base/define.go
@@ -6,9 +6,10 @@ type BaseRequest struct {
 }
 
 type Response struct {
-	Code ResponseCode `json:"status"`
-	Msg  string       `json:"msg"`
-	Data interface{}  `json:"data"`
+	Code        ResponseCode `json:"status"`
+	Msg         string       `json:"msg"`
+	Data        interface{}  `json:"data"`
+	ServiceTime int64        `json:"servicetime"`
 }
 
 //ResponseCode 状态返回码
@@ -23,11 +24,6 @@ const (
 	ResponseCode_Param_Invalid ResponseCode = 1002
 	ResponseCode_Data_Invalid  ResponseCode = 1003
 	ResponseCode_Data_NotExist ResponseCode = 1004
-
-	//2000~2999 运营
-	ResponseCode_Activity_NotExist   ResponseCode = 2000
-	ResponseCode_Activity_NotStarted ResponseCode = 2001
-	ResponseCode_Activity_Finish     ResponseCode = 2002
 )
 
 var responseCodeToMsg = map[ResponseCode]string{
@@ -39,11 +35,6 @@ var responseCodeToMsg = map[ResponseCode]string{
 	ResponseCode_Param_Invalid: "参数无效",
 	ResponseCode_Data_Invalid:  "数据无效",
 	ResponseCode_Data_NotExist: "数据不存在",
-
-	//2000~2999 运营
-	ResponseCode_Activity_NotExist:   "活动不存在",
-	ResponseCode_Activity_NotStarted: "活动没开始",
-	ResponseCode_Activity_Finish:     "活动已结束",
 }
 
 type DataStatusCode int
