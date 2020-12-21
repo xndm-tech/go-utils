78eb33342c7ffe091d0adc20fb6742c6aa11cdb9
xndm-recommend <1050099567@qq.com>
2020-12-21 20:04:28 +0800

    add MixListStr

--- tools/types/strs/mix.go
+++ tools/types/strs/mix.go
@@ -6,6 +6,19 @@ import (
 )
 
 // ints mix
+func MixListStr(s ...[]string) []string {
+	var count int
+	for _, item := range s {
+		count += len(item)
+	}
+	mix := make([]string, consts.ZERO, count)
+	for _, ss := range s {
+		mix = append(mix, ss...)
+	}
+	return UniqueStrs(mix)
+}
+
+// ints mix
 func MixListStrV2(s ...[]string) []string {
 	var count int
 	var maxLen int
@@ -13,7 +26,7 @@ func MixListStrV2(s ...[]string) []string {
 		count += len(item)
 		maxLen = maths.MaxInt(maxLen, len(item))
 	}
-	mix := make([]string, 0, count)
+	mix := make([]string, consts.ZERO, count)
 	for i := 0; i < maxLen; i++ {
 		for j := range s {
 			if i < len(s[j]) {
